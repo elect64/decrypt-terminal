ada5854bcdf8aef3f8eceef60c970daba4f787d3
elect <okezieelect4@gmail.com>
2026-09-13 11:34:14 +0100

    changed appscript link 0913

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzFpYLNWXgjLO-27Txb2E-Qi3sLSy2WcEpmnhKvq_oYdA9sbK24NbzIQ-Fu20n-X6uapw/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxS7N4a6BMeMiIdlosMb1WpzuBsB1Yad4K5htp6Hla-HxUUOLtvh6uqEYObn3wM_GYD8w/exec";
 
 let html5QrcodeScanner;
 let isProcessing = false;
